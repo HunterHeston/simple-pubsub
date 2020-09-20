66f75d444852132ba532007b868561501b028395
Hunter Heston <hunter_heston@yahoo.com>
2020-09-20 08:59:18 -0400

    removed extra topic registrations

--- src/test/receiverTest.js
+++ src/test/receiverTest.js
@@ -2,15 +2,6 @@ const { Receiver } = require("../receiver");
 
 const rec = new Receiver(3001, "localhost", 3000);
 
-rec.registerTopic("registration", (message) => {
-  console.log(`received message on topic test-topic: ${message}`);
-});
-rec.registerTopic("registration1", (message) => {
-  console.log(`received message on topic test-topic: ${message}`);
-});
-rec.registerTopic("registration2", (message) => {
-  console.log(`received message on topic test-topic: ${message}`);
-});
-rec.registerTopic("registration3", (message) => {
+rec.registerTopic("data-topic", (message) => {
   console.log(`received message on topic test-topic: ${message}`);
 });
