0d3cf8b9bb71c21d7dd8e3ab55872397b280f2de
Hunter Heston <hunter_heston@yahoo.com>
2020-09-25 12:46:09 -0400

    simple receiver example

---
+++ src/examples/listen_for_messages.js
@@ -0,0 +1,70 @@
+// local
+const { Receiver } = require("../receiver");
+const { LOG_INFO, LOG_WARN } = require("../util/log");
+
+////////////////////////////////////////////////////////////////////////////////
+function main() {
+  const args = process.argv.slice(2);
+
+  // default to reasonable values
+  let port = 3001;
+  let serverAddress = "localhost";
+  let serverPort = 3000;
+
+  // parse command-line arguments
+  //! This is fragile and for example purposes only.
+  for (const arg of args) {
+    const [command, value] = arg.split("=");
+
+    switch (command) {
+      case "--port":
+      case "-p":
+        port = parseInt(value);
+        break;
+
+      case "--server-address":
+      case "-s":
+        serverAddress = value;
+        break;
+
+      case "--server-port":
+      case "-sp":
+        serverPort = parseInt(value);
+        break;
+
+      case "--help":
+      case "-h":
+        LOG_INFO(
+          `main: accepted arguments: --port=<port> -p=<port> --server-address=<ip of server> -s=<ip of server> --server-port-<server-port> -sp=<server-port>`
+        );
+
+        //* is help is requested we should not proceed with execution.
+        return;
+
+      default:
+        LOG_WARN(
+          `main: invalid argument '${arg}' will be ignored: use -h for help`
+        );
+        break;
+    }
+  }
+
+  run(port, serverAddress, serverPort);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+function run(port, serverAddress, serverPort) {
+  const receiver = new Receiver(port, serverAddress, serverPort);
+
+  receiver.registerTopic("simple-ping", (message) => {
+    LOG_INFO(`run: received message on simple-topic`);
+    LOG_INFO(message);
+  });
+
+  receiver.start();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// start the main function
+////////////////////////////////////////////////////////////////////////////////
+main();
