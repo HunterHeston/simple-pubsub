192377e4d3b740c02ce238554e46d288a73214ec
Hunter Heston <hunter_heston@yahoo.com>
2020-09-25 13:15:20 -0400

    simple server example added

---
+++ src/examples/server_example.js
@@ -0,0 +1,52 @@
+// local
+const { Server } = require("../server");
+const { LOG_INFO, LOG_WARN } = require("../util/log");
+
+////////////////////////////////////////////////////////////////////////////////
+function main() {
+  // ignore first 2 arguments
+  const args = process.argv.slice(2);
+
+  // default to reasonable values
+  let port = 3000;
+
+  // parse command-line arguments
+  //! This is fragile and for example purposes only.
+  for (const arg of args) {
+    const [command, value] = arg.split("=");
+
+    switch (command) {
+      case "--port":
+      case "-p":
+        port = parseInt(value);
+        break;
+
+      case "--help":
+      case "-h":
+        LOG_INFO(`main: accepted arguments: --port=<port> -p=<port>`);
+
+        //* is help is requested we should not proceed with execution.
+        return;
+
+      default:
+        LOG_WARN(
+          `main: invalid argument '${arg}' will be ignored: use -h for help`
+        );
+        break;
+    }
+  }
+
+  // run the server
+  run(port);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+function run(port) {
+  const server = new Server(port);
+  server.start();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// execute main
+////////////////////////////////////////////////////////////////////////////////
+main();
